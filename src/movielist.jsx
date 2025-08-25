96fb5350ec41abdab95e2f14feee3b3a2df4cc01
Rohan Kasichhwa <rohankasichhwa@gmail.com>
2025-08-25 11:33:11 +0545

    page link agged

--- src/movielist.jsx
+++ src/movielist.jsx
@@ -2,16 +2,33 @@ import { useEffect, useState } from 'react';
 
 function MovieList({ category }) {
     const [movies, setMovies] = useState([]);
+    const [page, setPage] = useState(1);
 
     useEffect(() => {
-        fetch(`https://api.themoviedb.org/3/movie/${category}?api_key=3b847ccc51e3c965bbe1787d12943773&language=en-US&page=1`)
+        setPage(1)
+    }, [category])
+
+    useEffect(() => {
+        fetch(`https://api.themoviedb.org/3/movie/${category}?api_key=3b847ccc51e3c965bbe1787d12943773&language=en-US&page=${page}`)
             .then((res) => res.json())
             .then((data) => setMovies(data.results))
-    }, [category]);
+    }, [category, page]);
+
+    const nextpage = () => {
+        setPage(page + 1)
+    }
+    const prevpage = () => {
+        setPage(page - 1)
+    }
 
     return (
         <div className="App">
             <h1>{category}</h1>
+
+            <button disabled={page === 1} onClick={prevpage}>Previous</button>
+            <input type='number' min={1} onChange={(e) => { setPage(e.target.value) }} style={{ margin: '5px', width: "6ch" }} value={page} />
+            <button onClick={nextpage}>Next</button>
+
             <ol>
 
                 {movies.map((movie) => (
